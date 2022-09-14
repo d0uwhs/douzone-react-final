ba3022e89b867982d0299253e9c64c4b87848d2c
d0uwhs <d0uwhs@gmail.com>
2022-09-15 02:31:01 +0900

    Cookie Utils 작성 #14

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,13 +3,15 @@ import {StyledHeader} from "../style/styledHeader";
 import {Link} from "react-router-dom";
 
 const Header = () => {
+
+
     return (
         <StyledHeader>
             <ul>
                 <li>
                     <Link to="/">메인</Link>
                     <Link to="/menu">메뉴 보기</Link>
-                    <Link to="/login">로그인</Link>
+
                 </li>
             </ul>
         </StyledHeader>
