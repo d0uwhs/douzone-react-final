94dd032cdc0162b5070f2d09f8c80cda4463c545
d0uwhs <d0uwhs@gmail.com>
2022-09-14 14:54:16 +0900

    react-router-dom 의존 라이브러리 설치 및 Router 컴포넌트 작성 #8

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import './index.css'
+import './styles/fonts/fonts.css';
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="" element={<App/>}/>
+        </Routes>
+    </BrowserRouter>
+);
