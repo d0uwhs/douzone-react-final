bb0dbe88d4397b562bc41ad70ce84eede6d32279
d0uwhs <d0uwhs@gmail.com>
2022-09-14 15:17:50 +0900

    Change Branch Name

---
+++ src/pages/error/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/**
+ * Error Page.
+ * 에러 코드와, 에러 메시지를 받아 출력함.
+ * @param errorCode
+ * @param errorMsg
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ErrorPage = ({errorCode, errorMsg}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ErrorPage;
